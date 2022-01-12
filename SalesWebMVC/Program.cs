89078d4a0973cacc13f295b93f432b95c473c3e2
thierryxdp <thierrydutoit123@gmail.com>
2022-01-12 13:04:16 -0300

    CRUD Scaffolding

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SalesWebMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
